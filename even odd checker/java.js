3a61716013dc77e5daeb5cb2ce3a791ce724a9e0
rama0912 <ramaagarwalla4@gmail.com>
2025-06-23 13:01:31 +0530

    Add files via upload

---
+++ even odd checker/java.js
@@ -0,0 +1,8 @@
+function checkNumber() {
+  let number = document.getElementById("num").value;
+  if (num % 2 === 0) {
+    document.getElementById("result").innerText = "It's Even";
+  } else{
+    document.getElementById("result").innerText = "It's Odd."
+  }
+}
