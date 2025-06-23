431eed1211949be2a6c4eb864ddc1ca3b57149c9
rama0912 <ramaagarwalla4@gmail.com>
2025-06-23 13:03:15 +0530

    Update java.js

--- even odd checker/java.js
+++ even odd checker/java.js
@@ -1,7 +1,7 @@
 function checkNumber() {
   let number = document.getElementById("num").value;
   if (num % 2 === 0) {
-    document.getElementById("result").innerText = "It's Even";
+    document.getElementById("result").innerText = "It's Even number.";
   } else{
     document.getElementById("result").innerText = "It's Odd number."
   }
